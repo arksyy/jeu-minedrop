753d55d935d7751a5795870795197c011da2b2e6
Alexandre Roy <alexandre15roy@gmail.com>
2026-04-24 10:08:09 -0400

    feat: nombre de tokens et favicon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,36 @@
+import { useEffect } from 'react'
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Home from './pages/Home'
 import Jeu from './pages/Jeu'
 import Tokens from './pages/Tokens'
 
+const TITLES = {
+  '/': 'Minedrop',
+  '/jeu': 'Jeu - Minedrop',
+  '/tokens': 'Tokens - Minedrop',
+}
+
 function Layout() {
   const { pathname } = useLocation()
   const showPlatform = pathname === '/jeu'
 
+  useEffect(() => {
+    document.title = TITLES[pathname] ?? 'Minedrop'
+  }, [pathname])
+
   return (
     <div className="min-h-screen flex flex-col bg-sky-300 text-gray-900">
       <NavBar />
-      <main className="flex-1 flex flex-col items-center justify-end px-8 pt-8 pb-14">
+      <main className="flex-1 flex flex-col items-center justify-end px-8 pt-8 pb-12">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/jeu" element={<Jeu />} />
           <Route path="/tokens" element={<Tokens />} />
         </Routes>
       </main>
-      <div className="relative h-32 bg-green-500">
+      <div className="relative h-32 bg-green-500 flex items-center justify-center">
+        <span className="font-bold">Tokens : 1234</span>
         {showPlatform && (
           <div className="absolute bottom-full left-1/2 -translate-x-1/2 w-full max-w-xs h-10 bg-gray-300 border-2 border-gray-500" />
         )}
